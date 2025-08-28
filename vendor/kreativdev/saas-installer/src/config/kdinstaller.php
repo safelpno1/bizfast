bd0ea5dd3acf6b8d7893d9cbe68ae1e1ae9989c1
safelpno1 <101886483+safelpno1@users.noreply.github.com>
2025-08-28 13:18:53 +0700

    fristproject

---
+++ vendor/kreativdev/saas-installer/src/config/kdinstaller.php
@@ -0,0 +1,12 @@
+<?php 
+
+    return [
+        'item_name' => "Businesso",
+        "item_id" => 34880110,
+        "vension" => 3.5,
+        "released_on" => "13th October, 2024",
+        "broadcast_driver" => "log",
+        "email_api" => 'https://kreativdev.com/emailcollector/api/collect'
+    ];
+
+?>
